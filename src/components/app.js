9d636a22ff63ce5473c54c77580073703238a091
Thomas Dimperio <twdimperio@gmail.com>
2022-06-02 16:43:47 -0600

    add 'add user' functionality to newtaskform.js

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,13 @@ import ReactDom from 'react-dom';
 import Button from '@mui/material/Button';
 import NavBar from "./navbar";
 import NewTaskForm from "./newtaskform";
+import Login from './login'
 
 export default function App() {
   return (
     <div>
       <NewTaskForm></NewTaskForm>
+      <Login></Login>
     </div>
       
   );
--- src/components/newtaskform.js
+++ src/components/newtaskform.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
-import NativeSelect from '@mui/material/NativeSelect'
+import NativeSelect from '@mui/material/NativeSelect';
+import Container from '@mui/material/Container';
 
 export default function NewTaskForm() {
 
+  useEffect(() => {
+    fetch(`${url}/users`)
+      .then((r) => r.json())
+      .then(setUsers)
+  }, [])
+
+  const [users, setUsers] = useState([]);
   const [taskCategory, setTaskCategory] = useState("1");
   const [taskName, setTaskName] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
   const [taskPriority, setTaskPriority] = useState("Low");
+  const [taskUser, setTaskUser] = useState("");
 
   const url = "http://localhost:9292"
 
@@ -23,6 +32,7 @@ export default function NewTaskForm() {
       description: taskDescription,
       category_id: taskCategory,
       priority: taskPriority,
+      user_id: taskUser,
     }};
 
     console.log(submittedTask)
@@ -43,62 +53,79 @@ export default function NewTaskForm() {
       })
   }
   return (
-    <Box
-      backgroundColor='primary'
-      component='form'
-      sx={{
-        'a > :not(style)': { m: 1, width: '25ch'},
-      }}
-      noValidate
-      autoComplete = 'off'
-      onSubmit={handleFormSubmit}
-    >
-      <h1>Add Task</h1>
-      <TextField
-        required
-        id="outlined-required"
-        label="Task Name"
-        value={taskName}
-        onChange={(e) => setTaskName(e.target.value)}
-        
-      />
-      <TextField
-        id="outlined-multiline-flexible"
-        label="Task Description"
-        multiline
-        maxRows={4}
-        name="task-description"
-        value={taskDescription}
-        onChange={(e) => setTaskDescription(e.target.value)}
-      />
-      <FormControl>
-        <InputLabel variant="standard" htmlFor="uncontrolled-native">
-          Category
-        </InputLabel>
-        <NativeSelect
-          defaultValue={1}
-          onChange={(e) => setTaskCategory(e.target.value)}
-        >
-          <option value={1}>Home</option>
-          <option value={2}>Work</option>
-          <option value={3}>Personal</option>
-        </NativeSelect>
-      </FormControl>
-      <FormControl>
-        <InputLabel variant="standard" htmlFor="uncontrolled-native">
-          Priority
-        </InputLabel>
-        <NativeSelect
-          defaultValue={1}
-          onChange={(e) => setTaskPriority(e.target.value)}
-        >
-          <option value={1}>Low</option>
-          <option value={2}>Medium</option>
-          <option value={3}>High</option>
-        </NativeSelect>
-      </FormControl>
-        
-      <Button type="submit" variant="contained">Submit</Button>
-    </Box>
+    <Container component="main" maxWidth="xs">
+      <Box
+        backgroundColor='primary'
+        component='form'
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          aligntems: 'center',
+          maxWidth: "xs",
+        }}
+        noValidate
+        autoComplete = 'off'
+        onSubmit={handleFormSubmit}
+      >
+        <h1>Add Task</h1>
+        <TextField
+          required
+          id="outlined-required"
+          label="Task Name"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+
+        />
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Task Description"
+          multiline
+          maxRows={4}
+          name="task-description"
+          value={taskDescription}
+          onChange={(e) => setTaskDescription(e.target.value)}
+        />
+        <FormControl>
+          <InputLabel variant="standard" htmlFor="uncontrolled-native">
+            User
+          </InputLabel>
+          <NativeSelect
+            onChange={(e) => setTaskUser(e.target.value)}
+          >
+            {users.map((user) => 
+            <option key={user.id} value={user.id}>{user.name}</option>)}
+          </NativeSelect>
+        </FormControl>
+        <FormControl>
+          <InputLabel variant="standard" htmlFor="uncontrolled-native">
+            Category
+          </InputLabel>
+          <NativeSelect
+            defaultValue={1}
+            onChange={(e) => setTaskCategory(e.target.value)}
+          >
+            <option value={1}>Home</option>
+            <option value={2}>Work</option>
+            <option value={3}>Personal</option>
+          </NativeSelect>
+        </FormControl>
+        <FormControl>
+          <InputLabel variant="standard" htmlFor="uncontrolled-native">
+            Priority
+          </InputLabel>
+          <NativeSelect
+            defaultValue={1}
+            onChange={(e) => setTaskPriority(e.target.value)}
+          >
+            <option value={1}>Low</option>
+            <option value={2}>Medium</option>
+            <option value={3}>High</option>
+          </NativeSelect>
+        </FormControl>
+
+        <Button type="submit" variant="contained">Submit</Button>
+      </Box>
+    </Container>
   )
 }
