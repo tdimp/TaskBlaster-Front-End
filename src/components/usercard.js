51c93f7ace626c11c3c0e1d31068346646d69286
Thomas Dimperio <twdimperio@gmail.com>
2022-06-16 11:43:53 -0600

    remove delete button from UserCard, fix card height styling

--- src/components/usercard.js
+++ src/components/usercard.js
@@ -1,11 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
 import EditIcon from '@mui/icons-material/Edit';
 import { Link } from 'react-router-dom'
 
@@ -19,36 +16,19 @@ export default function UserCard({
   tasks
 }) {
 
-  const [open, setOpen] = useState(false)
-
   const userTasks = tasks.filter((task) => task.user_id == id)
-  const testArray = ["Test", "Testing", "Testering"]
   const categories = {1: "Home", 2: "Work", 3: "Personal"}
 
-  function handleOpenClick() {
-    setOpen(true);
-  }
-
-  function handleClose() {
-    setOpen(false);
-  }
-
-  function handleDeleteClick() {
-    console.log("click")
-    fetch(`http://localhost:9292/users/${id}`, {
-      method: "DELETE",
-    });
-    handleDelete(id)
-  }
   
   return (
     <Container>
       <Box
         sx={{
           width: 400,
-          height: 325,
+          height: '100%',
           backgroundColor: 'primary.dark',
-          margin: 10
+          margin: 10,
+          padding: 0.5
         }}
         >
         <Box sx={{
@@ -56,17 +36,9 @@ export default function UserCard({
           justifyContent: 'space-between',
         }}>
           <Link to={`/users/${id}/edit`}><Button variant={"contained"}><EditIcon /></Button></Link>
-          <Button variant="contained" onClick={handleOpenClick}><DeleteForeverIcon/></Button>
         </Box>
-        <Dialog onClose={handleClose} open={open}>
-          <DialogTitle>Delete User?</DialogTitle>
-          <DialogActions>
-            <Button onClick={handleClose}>No</Button>
-            <Button variant="contained" onClick={handleDeleteClick}>Yes</Button>
-          </DialogActions>
-        </Dialog>
         <h1>{name}</h1>
-        <ul>{tasks.filter((task) => task.user_id == id).map((t) => <li key={t.id}>{t.name} | {t.description} | {categories[t.category_id]} | {t.priority} </li>)}</ul>
+        <ul>{tasks.filter((task) => task.user_id == id).map((t) => <li key={t.id}><h3>{t.name}</h3> {t.description? t.description : "No desc"} | {categories[t.category_id]} | {t.priority} </li>)}</ul>
       </Box>
     </Container>
   )
