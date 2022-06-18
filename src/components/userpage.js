de3736e0fc233d7e560d5761f2b60eb741b863ec
Thomas Dimperio <twdimperio@gmail.com>
2022-06-18 14:51:54 -0600

    Minor revisions

--- src/components/userpage.js
+++ src/components/userpage.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import UserCard from './usercard';
 import Container from '@mui/material/Container';
